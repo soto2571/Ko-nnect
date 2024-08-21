6f918a00a603dfb1481c0d49512a821c08135b7f
Sebastian <sebastian.soto1649@gmail.com>
2024-08-21 19:40:39 -0400

    Auto commit

---
+++ website/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html", text="Testing", user="Sebastian")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Hola</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 character.', category='error')
+        elif len(firstName) <2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Password don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be 7 character.', category='error')
+        else:
+            # add user
+            flash('Account created.', category='success')
+
+    return render_template("sign_up.html")
+    
