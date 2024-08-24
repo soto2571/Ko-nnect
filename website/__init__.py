32a8b507a487a4ad72e3c51eac7d99930ff3e7b7
Sebastian <sebastian.soto1649@gmail.com>
2024-08-24 11:38:51 -0400

    Auto commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -19,12 +20,20 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
 def create_database(app):
-    with app.app_context():
-        if not path.exists('website/' + DB_NAME):
-            db.create_all()
-            print('Created Database!')
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
