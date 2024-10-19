3a10b407cad2312c1185975a8b361bd5fbb101cb
Sebastian <sebastian.soto1649@gmail.com>
2024-10-18 22:43:57 -0400

    Auto commit

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from .models import User, Schedule
 from . import db
 from werkzeug.security import generate_password_hash
+from datetime import datetime, date, timedelta
 
 views = Blueprint('views', __name__)
 
@@ -12,6 +13,26 @@ def home():
     if current_user.is_authenticated and current_user.is_admin:
         return redirect(url_for('admin.admin_panel'))
     
-    user_schedules = Schedule.query.filter_by(employee_id=current_user.id).all()
+    today = datetime.now()
+    today_date = today.strftime('%Y-%m-%d')  # Format today's date
+    dates = []
+    schedules = {}
+    today_schedule = None  # Initialize today_schedule
 
-    return render_template("home.html", user=current_user, schedules=user_schedules)
+    # Calculate today and next 13 days
+    for i in range(14):
+        date = today + timedelta(days=i)
+        formatted_date = date.strftime('%Y-%m-%d')  # Use this format for querying
+        schedules[formatted_date] = Schedule.query.filter(
+            Schedule.employee_id == current_user.id,
+            Schedule.shift_start >= date.replace(hour=0, minute=0, second=0, microsecond=0),
+            Schedule.shift_end < date.replace(hour=23, minute=59, second=59, microsecond=999999)
+        ).first()  # Get the first schedule for that day if it exists
+        
+        # Check if the current date is today
+        if formatted_date == today_date:
+            today_schedule = schedules[formatted_date]  # Set today_schedule for today
+
+        dates.append(date)
+
+    return render_template("home.html", user=current_user, dates=dates, schedules=schedules, today_schedule=today_schedule)
