1d30800b4b832848a529e6411e1e9abb6b1e783b
Sebastian <sebastian.soto1649@gmail.com>
2024-10-19 20:19:03 -0400

    Auto commit

---
+++ venv/lib/python3.12/site-packages/holidays/countries/vietnam.py
@@ -0,0 +1,84 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see AUTHORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from gettext import gettext as tr
+
+from holidays.groups import ChineseCalendarHolidays, InternationalHolidays
+from holidays.observed_holiday_base import ObservedHolidayBase, SAT_SUN_TO_NEXT_WORKDAY
+
+
+class Vietnam(ObservedHolidayBase, ChineseCalendarHolidays, InternationalHolidays):
+    """
+    https://publicholidays.vn/
+    http://vbpl.vn/TW/Pages/vbpqen-toanvan.aspx?ItemID=11013 Article.115
+    https://www.timeanddate.com/holidays/vietnam/
+    """
+
+    country = "VN"
+    # %s (observed).
+    observed_label = tr("%s (nghỉ bù)")
+    default_language = "vi"
+    supported_languages = ("en_US", "vi")
+
+    def __init__(self, *args, **kwargs):
+        ChineseCalendarHolidays.__init__(self)
+        InternationalHolidays.__init__(self)
+        kwargs.setdefault("observed_rule", SAT_SUN_TO_NEXT_WORKDAY)
+        super().__init__(*args, **kwargs)
+
+    def _populate_public_holidays(self):
+        dts_observed = set()
+
+        # New Year's Day
+        dts_observed.add(self._add_new_years_day(tr("Tết Dương lịch")))
+
+        # Lunar New Year's Eve
+        self._add_chinese_new_years_eve(tr("Giao thừa Tết Nguyên Đán"))
+
+        # Lunar New Year
+        self._add_chinese_new_years_day(tr("Tết Nguyên Đán"))
+
+        # Second Day of Lunar New Year
+        self._add_chinese_new_years_day_two(tr("Mùng hai Tết Nguyên Đán"))
+
+        # Third Day of Lunar New Year
+        self._add_chinese_new_years_day_three(tr("Mùng ba Tết Nguyên Đán"))
+
+        # Fourth Day of Lunar New Year
+        self._add_chinese_new_years_day_four(tr("Mùng bốn Tết Nguyên Đán"))
+
+        # Fifth Day of Lunar New Year
+        self._add_chinese_new_years_day_five(tr("Mùng năm Tết Nguyên Đán"))
+
+        if self._year >= 2007:
+            # Hung Kings' Commemoration Day
+            dts_observed.add(self._add_hung_kings_day(tr("Ngày Giỗ Tổ Hùng Vương")))
+
+        # Liberation Day/Reunification Day
+        dts_observed.add(self._add_holiday_apr_30(tr("Ngày Chiến thắng")))
+
+        # International Labor Day
+        dts_observed.add(self._add_labor_day(tr("Ngày Quốc tế Lao động")))
+
+        # National Day
+        dts_observed.add(self._add_holiday_sep_2(tr("Quốc khánh")))
+
+        if self.observed:
+            self._populate_observed(dts_observed)
+
+
+class VN(Vietnam):
+    pass
+
+
+class VNM(Vietnam):
+    pass
--- website/views.py
+++ website/views.py
@@ -1,49 +1,64 @@
-from flask import Blueprint, render_template, request, redirect, url_for, abort, flash
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
-from .models import User, Schedule
-from . import db
-from werkzeug.security import generate_password_hash
-from datetime import datetime, date, timedelta
+from .models import Schedule
+from datetime import datetime, timedelta
+import holidays  # Import the holidays library
 
 views = Blueprint('views', __name__)
-
-from datetime import datetime, timedelta
-from flask import redirect, url_for, render_template
-from flask_login import login_required, current_user
-from .models import Schedule  # Make sure to import your Schedule model
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if current_user.is_authenticated and current_user.is_admin:
         return redirect(url_for('admin.admin_panel'))
-    
+
     today = datetime.now()
-    today_date = today.strftime('%Y-%m-%d')  # Format today's date
+    today_date = today.strftime('%Y-%m-%d')
     dates = []
     schedules = {}
-    today_schedule = None  # Initialize today_schedule
-    calendar_dates = []  # Initialize for the calendar view
+    today_schedule = None
+    calendar_dates = []
 
-    # Calculate today and next 13 days for cards
+    # Get Puerto Rico holidays for the current year
+    pr_holidays = holidays.PR(years=today.year)
+
+    # Loop for the next 14 days for the schedule cards
     for i in range(14):
         date = today + timedelta(days=i)
-        formatted_date = date.strftime('%Y-%m-%d')  # Use this format for querying
+        formatted_date = date.strftime('%Y-%m-%d')
         schedules[formatted_date] = Schedule.query.filter(
             Schedule.employee_id == current_user.id,
             Schedule.shift_start >= date.replace(hour=0, minute=0, second=0, microsecond=0),
             Schedule.shift_end < date.replace(hour=23, minute=59, second=59, microsecond=999999)
-        ).first()  # Get the first schedule for that day if it exists
-        
-        # Check if the current date is today
+        ).first()
+
         if formatted_date == today_date:
-            today_schedule = schedules[formatted_date]  # Set today_schedule for today
+            today_schedule = schedules[formatted_date]
 
         dates.append(date)
 
-    # Calculate calendar dates for the next 4 weeks (28 days)
-    for i in range(-6, 15):  # Adjust the range to display the desired number of weeks
+    # Loop to generate the next 2 weeks (14 days) of calendar dates
+    for i in range(-6, 15):  # Adjust range as needed
         date = today + timedelta(days=i)
-        calendar_dates.append(date)
+        formatted_date = date.strftime('%Y-%m-%d')
 
-    return render_template("home.html", user=current_user, dates=dates, schedules=schedules, today_schedule=today_schedule, calendar_dates=calendar_dates)
+        is_holiday = date in pr_holidays
+        holiday_name = pr_holidays.get(date, '')
+        has_shift = formatted_date in schedules and schedules[formatted_date] is not None
+
+        # Append to the calendar dates with shift and holiday data
+        calendar_dates.append({
+            'date': date,
+            'is_holiday': is_holiday,
+            'holiday_name': holiday_name,
+            'has_shift': has_shift
+        })
+
+    return render_template(
+        "home.html", 
+        user=current_user, 
+        dates=dates, 
+        schedules=schedules, 
+        today_schedule=today_schedule, 
+        calendar_dates=calendar_dates
+    )
