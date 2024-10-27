50304f38d5b182fa9826496ab9150bcdcf80a00b
Sebastian <sebastian.soto1649@gmail.com>
2024-10-27 15:45:25 -0400

    Auto commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app --bind 0.0.0.0:$PORT
+web: python3 -m gunicorn main:app
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from website import create_app
-import os 
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run()
