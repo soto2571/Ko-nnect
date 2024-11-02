86dafc6c4eb415d6ff2b617f649dff2d9aa21191
Sebastian <sebastian.soto1649@gmail.com>
2024-11-02 10:20:21 -0400

    Auto commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from .models import Schedule
 from datetime import datetime, timedelta
@@ -9,6 +9,8 @@ views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET"])
 def landing_page():
+    if request.method == "POST":
+        return redirect(url_for('auth.login'))
     return render_template("landing_page.html")
 
 
