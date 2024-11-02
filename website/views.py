e8b6175af770c152f2de714404fc0f2997867009
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-11-01 22:14:21 -0400

    add landing

--- website/views.py
+++ website/views.py
@@ -4,16 +4,22 @@ from .models import Schedule
 from datetime import datetime, timedelta
 import holidays  # Import the holidays library
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-@views.route('/', methods=['GET', 'POST'])
+
+@views.route("/", methods=["GET"])
+def landing_page():
+    return render_template("landing_page.html")
+
+
+@views.route("/home", methods=["GET", "POST"])
 @login_required
 def home():
     if current_user.is_authenticated and current_user.is_admin:
-        return redirect(url_for('admin.admin_panel'))
+        return redirect(url_for("admin.admin_panel"))
 
     today = datetime.now()
-    today_date = today.strftime('%Y-%m-%d')
+    today_date = today.strftime("%Y-%m-%d")
     dates = []
     schedules = {}
     today_schedule = None
@@ -26,11 +32,13 @@ def home():
     # Loop for the next 14 days for the schedule cards
     for i in range(14):
         date = today + timedelta(days=i)
-        formatted_date = date.strftime('%Y-%m-%d')
+        formatted_date = date.strftime("%Y-%m-%d")
         schedules[formatted_date] = Schedule.query.filter(
             Schedule.employee_id == current_user.id,
-            Schedule.shift_start >= date.replace(hour=0, minute=0, second=0, microsecond=0),
-            Schedule.shift_end < date.replace(hour=23, minute=59, second=59, microsecond=999999)
+            Schedule.shift_start
+            >= date.replace(hour=0, minute=0, second=0, microsecond=0),
+            Schedule.shift_end
+            < date.replace(hour=23, minute=59, second=59, microsecond=999999),
         ).first()
 
         if formatted_date == today_date:
@@ -44,19 +52,23 @@ def home():
     # Loop to generate the next 2 weeks (14 days) of calendar dates from the fixed Sunday
     for i in range(21):  # Adjust the range to show the static calendar view
         date = start_of_week + timedelta(days=i)
-        formatted_date = date.strftime('%Y-%m-%d')
+        formatted_date = date.strftime("%Y-%m-%d")
 
         is_holiday = date in pr_holidays
-        holiday_name = pr_holidays.get(date, '')
-        has_shift = formatted_date in schedules and schedules[formatted_date] is not None
+        holiday_name = pr_holidays.get(date, "")
+        has_shift = (
+            formatted_date in schedules and schedules[formatted_date] is not None
+        )
 
         # Append to the calendar dates with shift and holiday data
-        calendar_dates.append({
-            'date': date,
-            'is_holiday': is_holiday,
-            'holiday_name': holiday_name,
-            'has_shift': has_shift
-        })
+        calendar_dates.append(
+            {
+                "date": date,
+                "is_holiday": is_holiday,
+                "holiday_name": holiday_name,
+                "has_shift": has_shift,
+            }
+        )
 
         # Calculate total hours worked in the current week
         if has_shift:
@@ -69,11 +81,11 @@ def home():
         total_hours_worked -= 1  # Deduct 1 hour for the break
 
     return render_template(
-        "home.html", 
-        user=current_user, 
-        dates=dates, 
-        schedules=schedules, 
-        today_schedule=today_schedule, 
+        "home.html",
+        user=current_user,
+        dates=dates,
+        schedules=schedules,
+        today_schedule=today_schedule,
         calendar_dates=calendar_dates,
-        total_hours_worked=total_hours_worked  # Pass total hours worked to the template
+        total_hours_worked=total_hours_worked,  # Pass total hours worked to the template
     )
